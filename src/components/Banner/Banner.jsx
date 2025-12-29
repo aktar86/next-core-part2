1bfd57143f4360354295e1ef3508b8ba56104964
Md Aktar Ahmed <amdaktar86@gmail.com>
2025-12-29 16:38:25 +0600

    first commit

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,20 @@
+"use client";
+import React from "react";
+import Title from "../Title/Title";
+
+const Banner = () => {
+  const handleBtn = () => {
+    alert("clicked");
+  };
+  return (
+    <div className="w-full  py-20 bg-blue-400 text-center">
+      <Title>This is Home Components</Title>
+
+      <button onClick={handleBtn} className="btn-primary rounded-full mt-5">
+        Show More
+      </button>
+    </div>
+  );
+};
+
+export default Banner;
